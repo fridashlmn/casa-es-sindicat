3666c8c244a147c7b3ba7f758b843c514ca19723
Frida <friederike.suhlmann@gmail.com>
2023-04-05 19:21:21 +0200

    feat: text and translations

--- pages/booking.vue
+++ pages/booking.vue
@@ -1,26 +1,27 @@
 <template>
   <div>
     <Stage
-      headline="Buchen"
+      :headline="$t('booking.stage.headline')"
+      :subline="$t('booking.stage.subline')"
       img-alt="image_pool"
       img-src="sunset.jpg"
-      subline="Hier steht eine griffige Subline"
     />
-    <div class="p-30">
-      <p class="text-center max-w-220 my-0 mx-auto pb-30">
+    <div class="px-6 py-16 md:p-16 lg:p-30">
+      <p class="text-center max-w-220 my-0 mx-auto pb-16 md:pb-16 lg:pb-30">
         {{ $t('booking.intro') }}
       </p>
-      <div class="flex justify-between">
-        <div>
-          <HeadlineComponent class="mb-8" level="M">Preise</HeadlineComponent>
+      <div class="flex flex-col lg:flex-row justify-evenly items-start">
+        <div class="max-w-111 mb-12">
+          <HeadlineComponent class="mb-8" level="M">
+            {{ $t('booking.price.title') }}
+          </HeadlineComponent>
           <span>
-            Preise bei einer Belegung mit bis zu 6 Erwachsenen sowie zwei
-            Kleinkindern (bis 5 Jahre)
+            {{ $t('booking.price.info') }}
           </span>
-          <ul class="listStyle ml-9 mb-30">
-            <li>Pro Tag: 750 € - 3 Nächte Mindestaufenthalt</li>
-            <li>Eine Woche: 5.100 €</li>
-            <li>Zwei Wochen: 10.000 €</li>
+          <ul class="listStyle ml-9">
+            <li>{{ $t('booking.price.day') }}</li>
+            <li>{{ $t('booking.price.week') }}</li>
+            <li>{{ $t('booking.price.weeks') }}</li>
           </ul>
         </div>
         <VueDatePicker
@@ -29,7 +30,6 @@
           :enable-time-picker="false"
           :highlight="highlightedDates"
           :inline="true"
-          :markers="markers"
           :max-date="maxDate"
           :min-date="new Date()"
           :month-change-on-scroll="false"
@@ -42,76 +42,34 @@
         />
       </div>
       <HeadlineComponent class="mb-8" level="M">
-        Mietbedingungen
+        {{ $t('booking.conditions.title') }}
       </HeadlineComponent>
-      <ul class="listStyle ml-9">
-        <li>
-          Wir bestätigen Ihnen per Mail die Anmietung der Finca. Damit die
-          Vermietung endgültig zustande gekommen ist, müssen Sie innerhalb einer
-          Woche nach Erhalt der Bestätigung 25 % der Miete als Anzahlung an uns
-          überweisen.
-        </li>
-        <li>
-          Die restlichen 75 % der Miete müssen bis 6 Wochen vor Beginn der
-          Mietzeit auf unserem Konto eingegangen sein.
-        </li>
-        <li>
-          Eine Mietkaution in Höhe von 1.000 € muss spätestens eine Woche vor
-          Mietbeginn auf unserem Konto eingegangen sein.
-        </li>
-        <li>
-          Bei der Anreise erfolgt, durch eine von uns beauftragte Person, die
-          Schlüsselübergabe sowie eine Einweisung in die Finca.
-        </li>
-        <li>
-          Am Tag der Abreise wird dieselbe Person den Schlüssel wieder
-          entgegennehmen und dabei die Finca auf ihren einwandfreien Zustand
-          überprüfen. Bei mängelfreiem Zustand werden wir Ihnen die Mietkaution
-          unverzüglich zurück überweisen.
-        </li>
-        <li>
-          Im Mietpreis enthalten sind Bettwäsche und Handtücher. Die
-          Endreinigung wird pauschal mit 220 € berechnet.
-        </li>
-        <li>
-          Die Finca darf nur durch die Anzahl Personen bewohnt werden, die bei
-          der Anmietung angegeben wurden. Maximal 6 bzw. 8 Personen sowie 2
-          Kleinkinder bis 3 Jahre.
-        </li>
-        <li>
-          Die Benutzung des Pools geschieht auf eigene Gefahr. Kinder sind von
-          den Eltern bei der Nutzung des Pools zu beaufsichtigen.
-        </li>
-        <li>
-          Am Tag der Anreise kann die Finca ab 16:00 Uhr bezogen werden. Am Tag
-          der Abreise muss die Finca bis 11:00 Uhr verlassen werden.
-        </li>
-        <li>Das Mitbringen von Haustieren ist untersagt.</li>
-        <li>Rauchen innerhalb des Hauses ist nicht gestattet.</li>
-        <li>Das Feiern von Partys auf der Finca ist nicht erlaubt.</li>
-      </ul>
+      <div class="listStyle ml-9" v-html="$t('booking.conditions.list')" />
     </div>
   </div>
 </template>
 <script lang="ts" setup>
 import VueDatePicker from '@vuepic/vue-datepicker'
 import '@vuepic/vue-datepicker/dist/main.css'
-import addYears from 'date-fns/addYears'
+import { addYears, eachDayOfInterval } from 'date-fns'
 import HeadlineComponent from '../components/Headline/index.vue'
 
 const date = ref()
 
 const maxDate = addYears(new Date(), 1)
+const getDatesInRange = (startDate: Date, endDate: Date): Date[] => {
+  return eachDayOfInterval({
+    start: new Date(startDate),
+    end: new Date(endDate),
+  })
+}
 
 const disabledDates = computed(() => {
   const today = new Date()
-
   const tomorrow = new Date(today)
   tomorrow.setDate(tomorrow.getDate() + 1)
-
   const afterTomorrow = new Date(tomorrow)
   afterTomorrow.setDate(tomorrow.getDate() + 1)
-
   return [
     new Date('2023-04-10'),
     new Date('2023-04-11'),
@@ -122,11 +80,11 @@ const disabledDates = computed(() => {
 })
 
 const highlightedDates = ref([
-  new Date('2023-04-05'),
-  new Date('2023-04-06'),
-  new Date('2023-04-07'),
-  new Date('2023-04-08'),
-  new Date('2023-04-09'),
+  ...getDatesInRange(new Date('2023-04-06'), new Date('2023-04-18')),
+  ...getDatesInRange(new Date('2023-07-09'), new Date('2023-09-09')),
+  ...getDatesInRange(new Date('2023-09-29'), new Date('2023-10-15')),
+  ...getDatesInRange(new Date('2023-11-17'), new Date('2023-12-03')),
+  ...getDatesInRange(new Date('2023-12-28'), new Date('2024-03-01')),
 ])
 </script>
 <script lang="ts">
