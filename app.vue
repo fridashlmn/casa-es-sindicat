7a9d139acba72cca4edec275097ca93500504a23
Frida <friederike.suhlmann@gmail.com>
2023-04-05 16:41:02 +0200

    feat: add page meta

--- app.vue
+++ app.vue
@@ -5,3 +5,22 @@
     </NuxtLayout>
   </div>
 </template>
+<script lang="ts" setup>
+const i18n = useI18n()
+
+const description = computed(() => {
+  return i18n.t('meta.description')
+})
+useHead({
+  title: 'Casa es Sindicat',
+  charset: 'utf-8',
+  viewport: 'width=device-width, initial-scale=1',
+  meta: [
+    {
+      hid: 'description',
+      name: 'description',
+      content: description,
+    },
+  ],
+})
+</script>
